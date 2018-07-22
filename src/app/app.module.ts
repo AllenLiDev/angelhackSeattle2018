97479df71e260ef59425dd69f2074bf55c998803
Garrett Yeo <garrett.mcyeo@gmail.com>
2018-07-21 21:50:53 -0700

    added different pages with infos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -8,9 +9,12 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { HikeOnePage } from '../pages/hike-one/hike-one';
+import { HikeTwoPage } from '../pages/hike-two/hike-two';
+import { HikeThreePage } from '../pages/hike-three/hike-three';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { IbmWeatherProvider } from '../providers/ibm-weather/ibm-weather';
 
 @NgModule({
   declarations: [
@@ -19,11 +23,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     TabsPage,
-    HikeOnePage
+    HikeOnePage,
+    HikeTwoPage,
+    HikeThreePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -32,12 +39,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     TabsPage,
-    HikeOnePage
+    HikeOnePage,
+    HikeTwoPage,
+    HikeThreePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    IbmWeatherProvider
   ]
 })
 export class AppModule {}
---
+++ src/pages/hike-three/hike-three.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HikeThreePage } from './hike-three';
+
+@NgModule({
+  declarations: [
+    HikeThreePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(HikeThreePage),
+  ],
+})
+export class HikeThreePageModule {}
